9ed1dbac559184ce76f7478919a954a628a43f7c
alexnicolae24 <nicolaealex246@yahoo.com>
2023-12-18 22:10:16 +0200

    Pop-up-uri adaugate.

--- src/EsriMapComponent.js
+++ src/EsriMapComponent.js
@@ -3,8 +3,9 @@ import WebMap from '@arcgis/core/WebMap';
 import MapView from '@arcgis/core/views/MapView';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 import Config from '@arcgis/core/config';
+import PopupTemplate from '@arcgis/core/PopupTemplate';
 
-import './EsriMapComponent.css'; // Make sure to import the CSS file for styling
+import './EsriMapComponent.css';
 
 const EsriMapComponent = () => {
   const mapViewNode = useRef(null);
@@ -16,30 +17,48 @@ const EsriMapComponent = () => {
       try {
         if (mapLoaded.current) return;
 
-        Config.apiKey = '';
+        Config.apiKey = 'AAPK3d737cc3995b41de949427b52d04f894Ujr4K1pyTeeqmvTK161wvqE6yVRI_bv7MLOWyOrKzXhG9DaHWeR4T-cXQfIGgmEW';
+
+        const popupFootballFields = new PopupTemplate({
+          title: '{name}',
+          content: [
+            {
+              type: 'fields',
+              fieldInfos: [
+                { fieldName: 'id', label: 'ID', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'description', label: 'Sport', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'Address', label: 'Address', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'Facilities', label: 'Facilities', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'No_fields', label: 'Fields', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'PhoneNumber', label: 'Phone Number', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+                { fieldName: 'Price', label: 'Price (per hour)', visible: true, isEditable: true, tooltip: '', format: null, stringFieldOption: 'text-box' },
+              ],
+            },
+          ],
+        });
 
         const map = new WebMap({
           basemap: 'arcgis/navigation',
         });
 
         const footballLayer = new FeatureLayer({
-          url:  "https://services6.arcgis.com/3T4q3twraXHKJdR1/ArcGIS/rest/services/Baze_Sportive/FeatureServer"
+          url: 'https://services6.arcgis.com/3T4q3twraXHKJdR1/ArcGIS/rest/services/Baze_Sportive/FeatureServer',
+          outFields: ['OBJECTID', 'name', 'Address', 'id', 'Facilities', 'description', 'Price', 'PhoneNumber', 'No_fields'],
+          popupTemplate: popupFootballFields,
         });
 
-        map.add(footballLayer);
+        map.add(footballLayer, 0);
 
         mapView.current = new MapView({
           container: mapViewNode.current,
-          center: [26.1025, 44.4268], // Bucharest coordinates
-          //center: [-118.73682450024377, 34.07817583063242],
+          center: [26.1025, 44.4268],
           zoom: 12,
           map: map,
         });
 
-        // Log errors to the console
         mapView.current.on('error', (error) => console.error('MapView error:', error));
 
-        await mapView.current.when(); // wait for the map to load
+        await mapView.current.when();
         console.log('ArcGIS map loaded');
         mapLoaded.current = true;
       } catch (error) {
@@ -49,7 +68,6 @@ const EsriMapComponent = () => {
 
     initializeMap();
 
-    // Cleanup function to destroy the MapView instance when the component is unmounted
     return () => {
       if (mapView.current) {
         mapView.current.destroy();
@@ -57,7 +75,7 @@ const EsriMapComponent = () => {
     };
   }, []);
 
-  return <div ref={mapViewNode} style={{ height: '500px', width: '100%' }}></div>;
+  return <div ref={mapViewNode} className="map-container"></div>;
 };
 
 export default EsriMapComponent;
